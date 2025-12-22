536be4a7dcf438cd388ac03ec47294057789394a
Esteban Aguililla Klein <esteban.aguililla.klein.pro@outlook.com>
2025-12-22 16:49:34 +0100

    Updating the nix flake

--- flake.nix
+++ flake.nix
@@ -2,42 +2,51 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
     pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
   };
 
-  outputs = { self, nixpkgs, pyproject-nix, ... }:
-    let 
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, pyproject-nix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system : 
+      let 
+        pkgs = import nixpkgs {inherit system;};
 
-      python = pkgs.python312;
-      project = pyproject-nix.lib.project.loadRequirementsTxt {
-        projectRoot = ./.;
-      };
-
-      pythonEnv =
-        assert project.validators.validateVersionConstraints { inherit python; } == { };
-        python.withPackages (ps:
-          (project.renderers.withPackages { inherit python; } ps)
-          ++ [ ps.pip ]   # optional but useful for debugging
-        );
-    in
-      {
-
-        packages.${system}.pythainer = python.buildPythonPackage {
-          pname = "pythainer"; 
-          version= "0.1";
-
-          src = ./.;
-
-          propagateBuildInputs = [
-            pythonEnv
-          ];
+	      python = pkgs.python3;
+	      pythonPackages = pkgs.python3Packages;
+        project = pyproject-nix.lib.project.loadRequirementsTxt {
+          projectRoot = ./.;
         };
 
-        devShells.${system}.default = pkgs.mkShell {
-          packages = [ pythonEnv ];
-        };
-      };
+        pythonEnv =
+          # assert project.validators.validateVersionConstraints { inherit python; } == { };
+          python.withPackages (ps:
+            (project.renderers.withPackages { inherit python; } ps)
+            ++ [ ps.pip ]   # useful for debugging
+          );
+      in
+        {
+
+          packages.pythainer = pythonPackages.buildPythonPackage {
+            pname = "pythainer"; 
+            version= "0.0.5";
+
+            src = ./.;
+
+            
+            propagatedBuildInputs = [
+              pythonEnv
+            ];
+            format = "pyproject";
+  	        build-system = [ pythonPackages.hatchling ];
+          };
+          packages.default = self.packages.${system}.pythainer;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              self.packages.${system}.pythainer
+            ];
+          };
+        }
+    );
 }
